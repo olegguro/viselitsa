ae5ff11c397b3ae7d1d35f74731ff2ce8449b8dd
Oleg Guro <ooleg6334@gmail.com>
2019-07-20 19:21:26 +0300

    Refactored code at WordReader

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -1,11 +1,9 @@
 class WordReader
   def read_from_file(file_name)
-    if !File.exist?(file_name)
-      return nil
-    end
-    file = File.new(file_name, "r:UTF-8")
-    lines = file.readlines
-    file.close
-    return lines.sample.chomp
+    return unless File.exist?(file_name)
+
+    lines = File.readlines(file_name, encoding: 'UTF-8')
+
+    lines.sample.chomp
   end
 end
